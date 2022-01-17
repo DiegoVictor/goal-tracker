e0051b702c9f2e962e976410e922baa20ef1db87
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-17 19:25:54 -0300

    Install router and configure navigation

---
+++ src/navigation/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function Navigation() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  );
+}
