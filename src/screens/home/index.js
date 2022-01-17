d095db399a39bcec010185d130135422c4aecb79
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-17 19:29:49 -0300

    Create Home page

---
+++ src/screens/home/index.js
@@ -0,0 +1,7 @@
+import { Container } from './styles';
+export default function Home() {
+  return (
+    <Container>
+    </Container>
+  );
+}
---
+++ src/screens/home/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+`;
