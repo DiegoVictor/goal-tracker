6c7e694b568146da8000521f3fe98ce54f0e0ecb
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-17 19:30:05 -0300

    Configure home page route

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from '../screens/home';
 export default function Navigation() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
