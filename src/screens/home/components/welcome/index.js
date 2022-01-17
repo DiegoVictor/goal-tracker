a6aad67310e08026d23d99fab6cd4a6764978f5e
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-17 19:35:43 -0300

    Create welcome component

---
+++ src/screens/home/components/welcome/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Orange } from '../../../../themes/commons';
+import { Subtitle } from '../../../../components/subtitle/styles';
+
+function Welcome({ children }) {
+  return (
+    <div>
+      <h3>
+        <Orange>Achieve your Goals</Orange>
+      </h3>
+      <Subtitle>Like a fox 🦊</Subtitle>
+
+      {children}
+    </div>
+  );
+}
+
+Welcome.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default Welcome;
