54e2bc7971506f3a0aafa53b0ac8fea54ba19db2
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-17 19:31:03 -0300

    Create yup schema validator

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,13 @@
+import * as yup from 'yup';
 import { Container } from './styles';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .required('Please type your best email')
+    .email('Must be a valid email'),
+});
+
 export default function Home() {
   return (
     <Container>
