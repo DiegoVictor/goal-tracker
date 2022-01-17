12ffa4ee4fe495a2d510ecaf69adc86217988647
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-17 19:24:54 -0300

    Create global style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Style } from './themes/default';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Style />
   </React.StrictMode>,
   document.getElementById('root')
 );
