42361e916b551531c3e561b67aa4fc668c0af9bd
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-17 19:26:18 -0300

    Import navigation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Navigation from './navigation';
 import { Style } from './themes/default';
 
 ReactDOM.render(
   <React.StrictMode>
     <Style />
+    <Navigation />
   </React.StrictMode>,
   document.getElementById('root')
 );
