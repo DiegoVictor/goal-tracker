7dd6dc39d1a5d7b1c7cb2dfeb9ec9100e7cdce1b
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-19 09:02:17 -0300

    Create header

---
+++ src/screens/dashboard/components/header/index.js
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+function Header() {
+  return (
+        <Container>
+        </Container>
+  );
+}
+
+export default Header;
---
+++ src/screens/dashboard/components/header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+
+  > div {
+    min-width: 400px;
+
+  }
+`;
