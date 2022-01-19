65a041662a2b36955aecca648973a4124662a14e
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-19 08:54:43 -0300

    Add dashboard page route

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Dashboard from '../screens/dashboard';
 import Home from '../screens/home';
+
 export default function Navigation() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
