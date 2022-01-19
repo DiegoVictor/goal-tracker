5e9c4011c083bcbac7473f124c82b1d8be49e5b4
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-19 08:54:27 -0300

    Create dashboard page

---
+++ src/screens/dashboard/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: auto;
+  min-width: 400px;
+  max-width: 900px;
+  padding: 50px 20px;
+  width: 100%;
+
+`;
