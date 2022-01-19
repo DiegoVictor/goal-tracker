5a929222b63643cd361398333c0aef9564203fd2
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-19 08:57:48 -0300

    Retrieve user key

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Container } from './styles';
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [userKey, setUserKey] = useState('');
+  useEffect(() => {
+    if (!location.state || !location.state?.key) {
+      navigate('/');
+    } else {
+      const { key } = location.state;
+
+      setUserKey(key);
+    }
+  }, []);
   return (
       <Container>
       </Container>
+  );
 }
