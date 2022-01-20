6a9ec14a153c800bb671895e847412e8741583db
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-20 08:43:57 -0300

    Configure search bar icon

--- src/screens/dashboard/components/search/index.js
+++ src/screens/dashboard/components/search/index.js
@@ -1,7 +1,18 @@
+import React, { useState } from 'react';
+import { IoIosSearch } from 'react-icons/io';
+
+import Button from '../../../../components/button';
 import Input from '../../../../components/input';
 
 function Search() {
   const [query, setQuery] = useState('');
+
+  const search = (
+    <Button type="button" disabled>
+      <IoIosSearch size={28} color="#3b3c45" />
+    </Button>
+  );
+  const [right, setRigth] = useState(search);
   return (
     <Input
       type="text"
