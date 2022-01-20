9e940271fd0486ab915ded416bbf18096afef2df
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-20 08:45:42 -0300

    Change icon and update the listing

--- src/screens/dashboard/components/search/index.js
+++ src/screens/dashboard/components/search/index.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { IoIosSearch } from 'react-icons/io';
+import React, { useContext, useEffect, useState } from 'react';
+import { IoIosClose, IoIosSearch } from 'react-icons/io';
 
 import Button from '../../../../components/button';
 import Input from '../../../../components/input';
@@ -16,6 +16,19 @@ function Search() {
   const [right, setRigth] = useState(search);
 
   const { reList } = useContext(GoalsContext);
+  useEffect(() => {
+    setRigth(search);
+
+    if (query.length > 0) {
+      setRigth(
+        <Button type="button" onClick={() => setQuery('')}>
+          <IoIosClose size={36} color="#3b3c45" />
+        </Button>
+      );
+    }
+
+    reList(query);
+  }, [query]);
 
   return (
     <Input
@@ -23,6 +36,7 @@ function Search() {
       placeholder="Search"
       maxlength="255"
       value={query}
+      right={right}
       onChange={(event) => setQuery(event.target.value)}
     />
   );
