dadcea9fb3d229629193eb1cc947fa3413ec2ea6
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-20 08:50:12 -0300

    List goals

--- src/screens/dashboard/components/goals/index.js
+++ src/screens/dashboard/components/goals/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Goal from '../goal';
 import { GoalsContext } from '../../../../contexts/GoalsContext';
 import { Container } from './styles';
 
@@ -8,6 +9,12 @@ function Goals() {
     <Container>
       <GoalsContext.Consumer>
         {({ goals }) => {
+          return (
+            <div>
+              {goals?.length > 0 &&
+                goals.map((goal) => <Goal {...goal} key={goal.id} />)}
+            </div>
+          );
         }}
       </GoalsContext.Consumer>
     </Container>
