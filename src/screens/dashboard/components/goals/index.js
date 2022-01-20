453cfbfad985c15737d9b64a9dd8c6b69e82edbf
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-20 08:49:11 -0300

    Read goals from context

--- src/screens/dashboard/components/goals/index.js
+++ src/screens/dashboard/components/goals/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { GoalsContext } from '../../../../contexts/GoalsContext';
+import { Container } from './styles';
 
 function Goals() {
   return (
+    <Container>
+      <GoalsContext.Consumer>
+        {({ goals }) => {
+        }}
+      </GoalsContext.Consumer>
+    </Container>
   );
 }
 
