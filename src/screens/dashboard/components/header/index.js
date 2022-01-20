986b0f913d15923ba79cb82d80266b7c0333d783
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-20 08:35:49 -0300

    Create logout button

--- src/screens/dashboard/components/header/index.js
+++ src/screens/dashboard/components/header/index.js
@@ -1,9 +1,26 @@
+import React, { useCallback } from 'react';
+import { IoIosLogOut } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
+import { FlexRight } from '../../../../themes/commons';
 import { GoalsContext } from '../../../../contexts/GoalsContext';
+import { Logout } from './styles';
 function Header() {
+  const navigate = useNavigate();
+  const logout = useCallback(() => {
+    localStorage.removeItem('current_session');
+    navigate('/');
+  }, []);
+
   return (
     <GoalsContext.Consumer>
       {({ setFormData }) => (
         <Container>
+          <div>
+            <FlexRight>
+              <Logout onClick={logout} title="Logout">
+                <IoIosLogOut size={22} color="#fff" />
+              </Logout>
+            </FlexRight>
         </Container>
       )}
     </GoalsContext.Consumer>
--- src/screens/dashboard/components/header/styles.js
+++ src/screens/dashboard/components/header/styles.js
@@ -7,5 +7,19 @@ export const Container = styled.div`
   > div {
     min-width: 400px;
 
+export const Logout = styled.button`
+  align-items: center;
+  background-color: #e33d8e;
+  border: 0px;
+  border-radius: 50%;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  padding-left: 3px;
+  padding-right: 0px;
+  height: 40px;
+  width: 40px;
+`;
+
   }
 `;
