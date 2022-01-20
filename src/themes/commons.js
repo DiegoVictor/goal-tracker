9af6e74ff915e5cfd461e5346f456d010e0a3874
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-20 08:34:04 -0300

    Create a flex div helper

--- src/themes/commons.js
+++ src/themes/commons.js
@@ -3,3 +3,8 @@ import styled from 'styled-components';
 export const Orange = styled.span`
   color: #f78042;
 `;
+
+export const FlexRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
