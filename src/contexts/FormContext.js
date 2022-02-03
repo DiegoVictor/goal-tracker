f052e4e8690554da811b061d4fb5fcf59e0b5e99
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-03 20:40:45 -0300

    Create form context

---
+++ src/contexts/FormContext.js
@@ -0,0 +1,30 @@
+import propTypes from 'prop-types';
+import React, { useMemo, useState } from 'react';
+
+export const INITIAL_GOAL_STATE = {
+  title: '',
+  description: '',
+  deadline: '',
+  tasks: [],
+  done: false,
+  completedAt: '',
+};
+
+export const FormContext = React.createContext({
+  formData: {},
+  setFormData: () => {},
+});
+
+export function FormContextProvider({ children }) {
+  const [formData, setFormData] = useState(null);
+
+  const context = useMemo(() => ({ formData, setFormData }), [formData]);
+
+  return (
+    <FormContext.Provider value={context}>{children}</FormContext.Provider>
+  );
+}
+
+FormContextProvider.propTypes = {
+  children: propTypes.element.isRequired,
+};
